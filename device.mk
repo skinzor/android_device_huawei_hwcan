6009620aa40ff689c5ad4ff318fe0f33e7f940b5
Willi Ye <williye97@gmail.com>
2017-02-05 19:07:22 +0100

    Kang cutils shim from kiwi

--- device.mk
+++ device.mk
@@ -95,6 +95,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9335.xml:system/etc/sound_trigger_mixer_paths_wcd9335.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# Compatibility
+PRODUCT_PACKAGES += \
+    libshim_cutils
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8953 \
