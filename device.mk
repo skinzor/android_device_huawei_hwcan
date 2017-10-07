48aa9ded89c7c97e07c0b7be5674b8b6f027aca7
skinzor <robertpopescu95@yahoo.com>
2017-10-07 21:28:53 +0300

    hwcan: add sensor_def_qcomdev.conf
    * Ill remove it from vendor.

--- device.mk
+++ device.mk
@@ -226,7 +226,8 @@ PRODUCT_PACKAGES += \
     sensors.nosensorhub.wrapper
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 # Thermal
 PRODUCT_COPY_FILES += \
