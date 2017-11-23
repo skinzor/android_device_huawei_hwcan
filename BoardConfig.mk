9ad004df893deca4b0c485ee719600a0bc933e2d
skinzor <robertpopescu95@yahoo.com>
2017-11-23 17:07:51 +0200

    hwcan: import RIL from rio
    * https://github.com/Huawei-Dev/android_device_huawei_rio/blob/7.1.1/ril/telephony/java/com/android/internal/telephony/HwQualcommRIL.java.
    * Testing purpose.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,6 +190,7 @@ BOARD_USES_QC_TIME_SERVICES := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
+BOARD_RIL_CLASS := ../../../device/huawei/hwcan/ril
 TARGET_RIL_VARIANT := caf
 
 # Root
