7446029c2e7e2a46f243b9d9521901ae0890c41d
skinzor <robertpopescu95@yahoo.com>
2017-12-10 11:31:58 +0200

    patches: remove all patches
    * Also disable apply.sh.
    * AEX already has substratum support.
    * The other two patches (su & updater) are meant for Lineage.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 add_lunch_combo aosp_hwcan-user
 add_lunch_combo aosp_hwcan-userdebug
 add_lunch_combo aosp_hwcan-eng
-sh device/huawei/hwcan/patches/apply.sh
+#sh device/huawei/hwcan/patches/apply.sh
