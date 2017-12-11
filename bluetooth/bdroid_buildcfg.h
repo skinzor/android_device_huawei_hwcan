4c59ae5d61dcd3c50e1c49fd59d86f6067634342
gt_o <cayne@gmx.de>
2017-12-11 14:21:57 +0100

    Fix bluetooth issue: showing very high energy consumption
    Change-Id: Ia79843599d4597e3d9d7f2bd3911993a8a62425c

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,6 +22,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "Huawei Nova"
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
