6a4f132f9130e475a01d70a63180f49daa296821
Artem Borisov <dedsa2002@gmail.com>
2017-12-22 11:45:58 +0300

    hwcan: Dex-preopt boot and system server
    WITH_DEXPREOPT_BOOT_IMG_ONLY was removed in 8.1 in favor of
    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY.
    
    Along with boot classpath it will also pre-optimize system server jars.
    
    Change-Id: Idd0ac1ac39a6c3d14637a974a0a99953c99af236

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,7 @@ ifeq ($(HOST_OS),linux)
     endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
