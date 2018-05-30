85426905ed1bfe2d213acf18632bc7ce7832cbe4
Robert Popescu <robertpopescu95@yahoo.com>
2018-05-30 11:51:15 -0400

    hwcan: fix typo

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -201,6 +201,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.huawei.wcnss.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.huawei.wcnss.rc
+LOCAL_SRC_FILES    := init.huawei.wcnss.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
