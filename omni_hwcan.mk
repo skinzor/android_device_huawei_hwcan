10e8d23d13c152500919295f83566afeea5216ba
Robert Popescu <robertpopescu95@yahoo.com>
2018-06-03 09:19:14 -0400

    hwcan: don't copy tzdata to fix build error
    * 2 September 2018: i don't remember what build error i had lol nor if i had it while building under Omni sources or minimal TWRP. I'll look into it someday. This should break the default (automatic) time zone, but it can be set manually.

--- omni_hwcan.mk
+++ omni_hwcan.mk
@@ -31,8 +31,8 @@ PRODUCT_PACKAGES += \
     charger_res_images
 
 # Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+#PRODUCT_COPY_FILES += \
+#    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hwcan
