ebf9c26775f02c55c7ae92c1c9f8deafa9ac7087
Robert Popescu <robertpopescu95@yahoo.com>
2018-06-22 14:05:22 -0400

    hwcan: try android.hardware.camera.provider@2.4-impl-legacy
    * Thanks to LuK1337 for the tip / suggestion.
    * TO DO: test if the camera still has the same behaviour.

--- device.mk
+++ device.mk
@@ -118,7 +118,7 @@ PRODUCT_PACKAGES += \
     libcamera_shim_mediaserver
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
