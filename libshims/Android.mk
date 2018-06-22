8f4e68d164a4f18c92b576044986dbb38c2f17fa
Robert Popescu <robertpopescu95@yahoo.com>
2018-06-22 11:00:30 -0400

    hwcan: libshims: fix build error
    * extern "C" is related to .cpp as far as i remember from LOS's IRC.
    
    libcamera_shim_display_intermediates/DisplayEventReceiver.o device/huawei/hwcan/libshims/DisplayEventReceiver.c"
    
    device/huawei/hwcan/libshims/DisplayEventReceiver.c:19:8: error: expected identifier or '('
    extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE();
           ^
    device/huawei/hwcan/libshims/DisplayEventReceiver.c:21:8: error: expected identifier or '('
    extern "C" void _ZN7android20DisplayEventReceiverC1Ev() {

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -21,7 +21,7 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := DisplayEventReceiver.c
+LOCAL_SRC_FILES := DisplayEventReceiver.cpp
 LOCAL_MODULE := libcamera_shim_display
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
