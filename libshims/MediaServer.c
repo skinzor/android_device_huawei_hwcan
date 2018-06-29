c989c37a4ffe0ddb9a58d314adc34de0e9140368
Robert Popescu <robertpopescu95@yahoo.com>
2018-06-29 10:10:11 -0400

    Revert "hwcan: try exif shim from alice"
    * It didn't change anything.
    
    This reverts commit dd3ee82bf37e1e24ad3b2eac7bba9d0db38bc912.

--- libshims/MediaServer.c
+++ libshims/MediaServer.c
@@ -17,14 +17,4 @@
 /* TODO: Actually provide implementations for these function! */
 
 /* libcamera_post_mediaserver */
-#include <stdio.h>
-
-int __srget(FILE *fp)
-{
-	return EOF;
-}
-
-void exif_entry_gps_initialize(void)
-{
-    return;
-}
+extern int exif_entry_gps_initialize() { return 0; }
