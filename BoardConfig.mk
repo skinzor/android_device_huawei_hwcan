1eb37c1cbf1a48ad87f5225e9fe9852b28640ba7
Robert Popescu <robertpopescu95@yahoo.com>
2018-07-01 10:41:01 -0400

    hwcan: don't link libshim_cutils to libcutils
    * Because it's crashing the WebView.
    
    E WebViewZygoteInit: Caused by: java.lang.UnsatisfiedLinkError: dlopen failed: library "libshim_cutils.so" not found
    
    * Thanks to javelinanddart for the tip:
    
    https://review.lineageos.org/#/c/LineageOS/android_device_samsung_klte-common/+/217506/
    
    "tl;dr -- Stop shimming libcutils and shim the libs that need the symbols directly."
    
    ==============================================================
    
    * There's the following error when i'm linking the shims through the old way (libshim_cutils to libcutils), along with the other symbols which shims were supposed to fix:
    
    E HAL     : dlopen failed: cannot locate symbol "__android_logPower_print" referenced by "/system/lib64/hw/fingerprint.vendor.msm8953.so"...
    
    * There's more than fingerprint's symbol in libshim_cutils though. It has parts related to the kernel too, so there might be a problem regarding that, but i'll see how it goes. The fingerprint blob wasn't complaining about that missing symbol when the shim was linked to libcutils, but probably it didn't complain because the fingerprint blob has libcutils.so as dependency (checked shared libraries). One of the other dependencies is libhwlog.so (which is already in the vendor), so maybe it would be better to link the shim to that one. Again, i'll see how it goes.
    
    ldd-arm fingerprint.vendor.msm8953.so
    libcutils.so
    lib_fpc_tac_shared.so
    libimonitor.so
    libhwlog.so
    libc++.so
    libdl.so
    libc.so
    libm.so
    
    ==============================================================
    
    * The commit:
    
    "klte-common: Rework shims
    
    * Recent binder changes caused our approach of shimming libcutils to
      re-add the non-inlined android_atomic* methods to start getting
      slapped, at least with NDK apps. This looks oddly similar to the
      heads-up we received in 7.0, so no clue why it let us do it in 8.*
      prior to now.
    * See: https://developer.android.com/about/versions/nougat/android-7.0-changes#ndk
    * We did this because all of the libsec-ril* blobs and libuiblur
      needed it and doing each and every libsec-ril seemed fragile and
      fraught with the potential for error, but "fragile and fraught with
      the potential for error" is better than "phone breaking". And blur
      doesn't seem to ever be coming back.
    * We'll leave the "standard" libsec-ril.so here and let the
      individual devices handle anything specific if they have variant-
      specific blobs"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,8 +213,7 @@ USE_SENSOR_MULTI_HAL := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib64/libcutils.so|libshim_cutils.so \
-    /system/lib/libcutils.so|libshim_cutils.so \
+    /system/lib64/hw/fingerprint.vendor.msm8953.so|libshim_cutils.so \
     /system/lib/hw/camera.msm8953.so|/system/vendor/lib/libshim_camera.so \
     /system/lib/hw/camera.msm8953.so|libshims_sensorlistener.so \
     /system/lib/libcamera_post_mediaserver.so|libcamera_shim_mediaserver.so \
