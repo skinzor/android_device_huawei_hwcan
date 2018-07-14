37d6a6afe5d3310dd588c1eafa4d4a1d4381332c
Robert Popescu <robertpopescu95@yahoo.com>
2018-07-14 22:40:56 +0300

    hwcan: don't inherit phone-xxhdpi-3072-hwui-memory.mk
    * Because it no longer exists.
    * Fix build error (it was complaining that it is missing).
    
    * See here: https://github.com/LineageOS/android_frameworks_native/commit/74b51bcc305f0335060c1e049a61e1474a0ef045
    
    Quote from the above link:
    
    "* In Android 8.1 and later, only the ro.zygote.disable_gl_preload
       property still applies. All other properties have been removed.
       Reference: https://source.android.com/devices/graphics/renderer"
    
    ==============================
    A useless note below:
    
    * When i've tried to build AOSP N for the first time, i've added those props in device.mk since AOSP doesn't have phone-xxhdpi-3072-hwui-memory/mk nor hone-xxhdpi-3072-dalvik-heap.mk
    
    Dalvik
    PRODUCT_PROPERTY_OVERRIDES += \
        dalvik.vm.heapstartsize=8m \
        dalvik.vm.heapgrowthlimit=288m \
        dalvik.vm.heapsize=768m \
        dalvik.vm.heaptargetutilization=0.75 \
        dalvik.vm.heapminfree=512k \
        dalvik.vm.heapmaxfree=8m
    
    Hwui
    PRODUCT_PROPERTY_OVERRIDES += \
        ro.hwui.texture_cache_size=72 \
        ro.hwui.layer_cache_size=48 \
        ro.hwui.path_cache_size=32 \
        ro.hwui.gradient_cache_size=1 \
        ro.hwui.drop_shadow_cache_size=6 \
        ro.hwui.r_buffer_cache_size=8 \
        ro.hwui.texture_cache_flushrate=0.4 \
        ro.hwui.text_small_cache_width=1024 \
        ro.hwui.text_small_cache_height=1024 \
        ro.hwui.text_large_cache_width=2048 \
        ro.hwui.text_large_cache_height=1024

--- device.mk
+++ device.mk
@@ -71,7 +71,6 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # ANT
 PRODUCT_PACKAGES += \
