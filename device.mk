ce520dad1ca609f2edeb82f5a99521b3101002f4
Robert Popescu <robertpopescu95@yahoo.com>
2018-08-16 14:41:21 +0300

    hwcan: build camera.provider@2.4-impl instead legacy one

--- device.mk
+++ device.mk
@@ -117,8 +117,9 @@ PRODUCT_PACKAGES += \
     libcamera_shim_mediaserver
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
     Snap \
