38f7baadca3ad8270980897fa92ab2a8303bf84a
Robert Popescu <robertpopescu95@yahoo.com>
2018-08-25 11:16:18 +0300

    hwcan: build android.hardware.biometrics.fingerprint@1.0-service.hwcan

--- device.mk
+++ device.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service
+    android.hardware.biometrics.fingerprint@1.0-service.hwcan
 
 # FM
 PRODUCT_PACKAGES += \
