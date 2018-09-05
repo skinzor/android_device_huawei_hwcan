91a58e51e9458e48ea84169a4fa3e0cf7d3e4e23
Robert Popescu <robertpopescu95@yahoo.com>
2018-09-05 23:12:05 +0300

    hwcan: include device's sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
